43790a7eb901b06f6fa2c2e73bd3bfa93a52759d
devinvolk@comcast.net <devinvolk@Devins-MacBook-Air.local>
2023-02-08 20:26:03 -0800

    first commit

---
+++ module.py
@@ -0,0 +1,10 @@
+from math import pi
+
+def circumference_circle_radius(radius):
+    return 2*pi*radius
+
+def circumference_circle_diameter(diameter):
+    return pi*diameter
+
+def area_house(length, width):
+    return length*width
